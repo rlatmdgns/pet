2a25184c9e9b5a471bbb7725d23f8acd27a3ea0c
김승훈 <rlatmdgns94@naver.com>
2024-04-20 02:27:51 +0900

    feat: 불필요한 코드 제거

--- src/features/login/api/login.ts
+++
@@ -1,6 +0,0 @@
-import { API_URL } from '@/src/shared/constants/api'
-import { http } from '@/src/shared/lib/http'
-
-export const login = (social: 'GOOGLE' | 'KAKAO') => {
-  return http.get(API_URL.LOGIN(social))
-}
