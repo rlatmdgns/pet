7a40deab28e8274e87e6957b31bc258c0bfb8ddc
김승훈 <rlatmdgns94@naver.com>
2024-04-27 02:24:12 +0900

    test: 파일 업로드

--- src/shared/lib/helper.ts
+++ src/shared/lib/helper.ts
@@ -2,16 +2,14 @@ import Cookies from 'js-cookie'
 
 export const getToken = () => Cookies.get('accessToken')
 
+console.log(getToken())
+
 interface SteadResponse extends Response {
   data?: any
 }
 
 export const handleResponse = async <T>(response: SteadResponse) => {
   const data = await response.json()
-
-  if (!response.ok) {
-    Cookies.remove('token')
-  }
 
   if (!response.ok) {
     console.error(data)
